21cb03ecf93669ec25910ea2074c5a6f191dbc17
Rakshita <rakshita@example.com>
2020-05-04 10:54:35 +0530

    ~ Task adding list

--- app/src/main/java/com/example/me/MainActivity.java
+++ app/src/main/java/com/example/me/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.me;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,12 +44,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigation;
-    TextView Temp , Date, City, Description;
+    TextView Temp, Date, City, Description;
+
+    DBhelper dBhelper;
+    ArrayAdapter<String> mAdapter;
+    ListView listTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +67,35 @@ public class MainActivity extends AppCompatActivity {
         City = findViewById(R.id.City);
         Description = findViewById(R.id.Temperature_Description);
 
+        dBhelper = new DBhelper(this);
+        listTask = findViewById(R.id.ListViewHome);
+
+
+        /*button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNum1 = "8218098475";
+                String phoneNumber = "tel:" + phoneNum1;
+
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse(phoneNumber));
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    //Toast.makeText(MainActivity.this,"Calls something I guess!", Toast.LENGTH_LONG).show();
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(intent);
+            }
+        });*/
+
         loadFragment(new home());
+
 
         findWeather();
 
@@ -53,8 +104,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         Fragment selectedFragment = null;
-                        switch (item.getItemId())
-                        {
+                        switch (item.getItemId()) {
                             case R.id.navigation_home:
                                 selectedFragment = new home();
                                 loadFragment(selectedFragment);
@@ -86,16 +136,75 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigation.setOnNavigationItemSelectedListener(navigation);
     }
 
+    private void loadTaskList() {
+        ArrayList<String> taskList = dBhelper.getTaskList();
+        if(mAdapter == null) {
+            mAdapter = new ArrayAdapter<String>(this,R.layout.row,R.id.taskTitle,taskList);
+            listTask.setAdapter(mAdapter);
+        }
+        else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_to_do_list,menu);
+
+        Drawable icon = menu.getItem(0).getIcon();
+        icon.mutate();
+        icon.setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add New Task")
+                        .setMessage("What is the task?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                dBhelper.insertNewTask(task);
+                                loadTaskList();
+
+                            }
+                        })
+                        .setNegativeButton("Cancel",null)
+                        .create();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deleteTask(View view) {
+        View parent = (View) view.getParent();
+        TextView taskTextViews = findViewById(R.id.taskTitle);
+        String task = String.valueOf(taskTextViews.getText());
+        dBhelper.deleteTask(task);
+        loadTaskList();
+    }
+
+
+
     private void loadFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container,fragment);
+        transaction.replace(R.id.fragment_container, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
 
-    public void findWeather()
-    {
-        String url = "http://api.openweathermap.org/data/2.5/weather?q=delhi,In&appid=5fbef19c2ae09857e08b7f1e452f819c";
+    public void findWeather() {
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=delhi,In&appid=5fbef19c2ae09857e08b7f1e452f819c&units=imperial";
 
         JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -103,10 +212,10 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONObject main_object = response.getJSONObject("main");
                     JSONArray array = response.getJSONArray("Weather");
-                    JSONObject object = array.getJSONObject(0);
+                    JSONObject object = array.getJSONObject(8);
                     String temp = String.valueOf(main_object.getDouble("temp"));
-                    String description = object.getString("description");
-                    String city = response.getString("name");
+                    String description = array.getString(2);
+                    String city = object.getString("name");
 
                     //Temp.setText(temp);
                     Description.setText(description);
@@ -119,15 +228,14 @@ public class MainActivity extends AppCompatActivity {
                     Date.setText(formatted_date);
 
                     double temp_int = Double.parseDouble(temp);
-                    double centi = (temp_int-32)/1.8000;
+                    double centi = (temp_int - 32) / 1.8000;
                     centi = Math.round(centi);
-                    int i = (int)centi;
+                    int i = (int) centi;
                     Temp.setText(String.valueOf(i));
 
-                } catch (JSONException e)
-                    {
-                        e.printStackTrace();
-                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }, new Response.ErrorListener() {
             @Override
@@ -148,8 +256,40 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"FAB Clicked!",Toast.LENGTH_LONG).show();
+
+                Toast.makeText(MainActivity.this, "FAB Clicked!", Toast.LENGTH_LONG).show();
             }
         });
     }
+
+    /*public void Call1(View view)
+    {
+        String phoneNumber="8755555444";
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse(phoneNumber));
+
+        startActivity(callIntent);
+    }
+
+    public void Call2(View view)
+    {
+        String phoneNumber="8218098475";
+
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse(phoneNumber));
+
+        startActivity(callIntent);
+    }
+
+    public void Call3(View view)
+    {
+        String phoneNumber="9811921380";
+
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse(phoneNumber));
+
+        startActivity(callIntent);
+    }*/
+
+
 }
--- app/src/main/java/com/example/me/calls.java
+++ app/src/main/java/com/example/me/calls.java
@@ -1,18 +1,88 @@
 package com.example.me;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class calls extends Fragment {
 
+    private View view;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View viewInflater = inflater.inflate(R.layout.fragment_call, container, false);
+
+        /*Button button1 = view.findViewById(R.id.Call1);
+        Button button2 = view.findViewById(R.id.Call2);
+        Button button3 = view.findViewById(R.id.Call3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNum1 = "8218098475";
+                String phoneNumber = "tel:" + phoneNum1;
+
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse(phoneNumber));
+
+                startActivity(intent);
+            }
+        });
+*/
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_call, container, false);
+        return viewInflater;
     }
+
+    /*public void Call1(View view)
+    {
+        String phoneNumber="8755555444";
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse(phoneNumber));
+        startActivity(callIntent);
+
+        startActivity(callIntent);
+    }
+
+    public void Call2(View view)
+    {
+        String phoneNumber="8218098475";
+
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse(phoneNumber));
+        startActivity(callIntent);
+
+        startActivity(callIntent);
+    }
+
+    public void Call3(View view)
+    {
+        String phoneNumber="9811921380";
+
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse(phoneNumber));
+        startActivity(callIntent);
+
+        startActivity(callIntent);
+    }
+*/
 }
 
